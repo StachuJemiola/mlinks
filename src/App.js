dc3f6e30e7d324e1942670deb5cef3ec22c39dbd
mkl <mkl2@atlantyda.pl>
2018-02-27 23:20:47 +0100

    Search field added.

--- src/App.js
+++ src/App.js
@@ -3,26 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 import links from './links.json';
 
+class SearchTag extends Component {
+  handleChange(e) {
+    this.props.onChange(e.target.value);
+  }
+  render() {
+    return <input onChange={this.handleChange.bind(this)} />
+  }
+}
+
 const TagList = ({tags}) => {
-  const tagDivs = tags.map((tag) => <div class="tag-name">{tag}</div>);
-  return <div class="tag-list">{tagDivs}</div>
+  const tagDivs = tags.map((tag) => <div className="tag-name">{tag}</div>);
+  return <div className="tag-list">{tagDivs}</div>
 }
 
 class LinkList extends Component {
   render() {
     return (
-      links.map((link) => (
-        <p>
-        {new Date(link.date * 1000).toLocaleString('pl-PL')}
-        <a href={link.url} target="_blank">{link.title}</a>
+      this.props.links.map((link) => (
+        <div className="link-box">
+          <div className="link-date">{new Date(link.date * 1000).toLocaleString('pl-PL')}</div>
+          <a href={link.url} target="_blank">{link.title}</a>
           <TagList tags={link.tags} />
-        </p>
+        </div>
       ))
     );
   }
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {links: links.sort((e1, e2) => e1.date - e2.date)};
+  }
+  tagFilter(tag) {
+    if (tag) {
+      this.setState({links: links.filter((e) => e.tags.includes(tag))});
+    }
+    else {
+      this.setState({links})
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -30,7 +51,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <LinkList />
+        <SearchTag onChange={this.tagFilter.bind(this)} />
+        <LinkList links={this.state.links} />
       </div>
     );
   }
